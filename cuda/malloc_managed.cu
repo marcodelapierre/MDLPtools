1c67708b37df0af1028de16d35ea14c13739b981
Marco De La Pierre <marco.delapierre@gmail.com>
2019-10-31 12:36:01 +0800

    added cuda/malloc_managed

---
+++ cuda/malloc_managed.cu
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+__global__ void deviceKernel(int *a, int N)
+{
+  int idx = threadIdx.x + blockIdx.x * blockDim.x;
+  int stride = blockDim.x * gridDim.x;
+
+  for (int i = idx; i < N; i += stride)
+  {
+    a[i] = 1;
+  }
+}
+
+void hostFunction(int *a, int N)
+{
+  for (int i = 0; i < N; ++i)
+  {
+    a[i] = 1;
+  }
+}
+
+int main()
+{
+  int N = 2<<24;
+  size_t size = N * sizeof(int);
+  int *a;
+  cudaMallocManaged(&a, size);
+
+  //hostFunction(a,N);
+  deviceKernel<<<80,32>>>(a,N);
+  cudaDeviceSynchronize();
+
+  printf("%d\n",a[0]);
+
+  cudaFree(a);
+} 
